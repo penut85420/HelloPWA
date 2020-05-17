609ac319dc6c7af9670bb81c201ef5600dfc18f6
PenutChen <penut85420@gmail.com>
2020-05-17 22:24:38 +0800

    Update full function

--- js/main.js
+++ js/main.js
@@ -4,4 +4,22 @@ window.onload = () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./sw.js');
     }
+};
+
+
+class go_calc {
+    constructor() {
+        if (calculator.go.value == "÷") {
+            calculator.go.value = "=";
+            calculator.calculation.value += "÷"
+        } else {
+            var n = calculator.calculation.value.split("÷");
+            var a = parseInt(n[0]);
+            var b = parseInt(n[1]);
+            var q = Math.floor(a / b);
+            var r = a % b;
+            calculator.Q.value = q;
+            calculator.R.value = r;
+        }
+    }
 }
